9e742e01352ea1ec91bc49d2d34d51a8c0fb61cb
Bruno Sastre <bsastre@genexus.com>
2022-11-22 10:03:45 -0300

    update blocks

--- src/components/gwds-blocks/gwds-blocks.tsx
+++ src/components/gwds-blocks/gwds-blocks.tsx
@@ -34,6 +34,8 @@ export class GwdsBlocks {
 
   @State() textColor: string = null;
   @State() colClasses: string = 'col-12 col-lg-8';
+  @State() mainWrapperClasses: string = 'gwds-blocks__main-wrapper col-12';
+  @State() gridClasses: string = 'col-12';
 
   componentWillLoad() {
     //define text color based on contrast with the background
@@ -41,6 +43,8 @@ export class GwdsBlocks {
 
     if (this.display === 'grid-aside') {
       this.colClasses = 'col-12';
+      this.mainWrapperClasses = 'gwds-blocks__main-wrapper col-12 col-lg-5';
+      this.gridClasses = 'col-12 col-lg-7';
     }
   }
 
@@ -69,28 +73,30 @@ export class GwdsBlocks {
       >
         <section class="section">
           <div class={{ 'container': true, 'container--main': true }}>
-            <div class="gwds-blocks__main-wrapper">
-              <div class="row">
-                <div class={this.colClasses}>
-                  {this.mainTitle ? <h2 class="h2">{this.mainTitle}</h2> : null}
-                  {this.description ? <div innerHTML={marked.parse(this.description)}></div> : null}
+            <div class="row">
+              <div class={this.mainWrapperClasses}>
+                <div class="row">
+                  <div class={this.colClasses}>
+                    {this.mainTitle ? <h2 class="h2">{this.mainTitle}</h2> : null}
+                    {this.description ? <div innerHTML={marked.parse(this.description)}></div> : null}
+                  </div>
+                </div>
+                <div class="gwds-blocks__buttons-wrapper">
+                  {this.firstButtonLabel && this.firstButtonUrl ? (
+                    <gwds-button label={this.firstButtonLabel} type={this.firstButtonType} url={this.firstButtonUrl} target={this.firstButtonTarget}></gwds-button>
+                  ) : null}
+                  {this.secondButtonLabel && this.secondButtonUrl ? (
+                    <gwds-button label={this.secondButtonLabel} type={this.secondButtonType} url={this.secondButtonUrl} target={this.secondButtonTarget}></gwds-button>
+                  ) : null}
+                  {this.thirdButtonLabel && this.thirdButtonUrl ? (
+                    <gwds-button label={this.thirdButtonLabel} type={this.thirdButtonType} url={this.thirdButtonUrl} target={this.thirdButtonTarget}></gwds-button>
+                  ) : null}
                 </div>
               </div>
-              <div class="gwds-blocks__buttons-wrapper">
-                {this.firstButtonLabel && this.firstButtonUrl ? (
-                  <gwds-button label={this.firstButtonLabel} type={this.firstButtonType} url={this.firstButtonUrl} target={this.firstButtonTarget}></gwds-button>
-                ) : null}
-                {this.secondButtonLabel && this.secondButtonUrl ? (
-                  <gwds-button label={this.secondButtonLabel} type={this.secondButtonType} url={this.secondButtonUrl} target={this.secondButtonTarget}></gwds-button>
-                ) : null}
-                {this.thirdButtonLabel && this.thirdButtonUrl ? (
-                  <gwds-button label={this.thirdButtonLabel} type={this.thirdButtonType} url={this.thirdButtonUrl} target={this.thirdButtonTarget}></gwds-button>
-                ) : null}
-              </div>
+              <gwds-grid perRow={this.perRow()} class={this.gridClasses}>
+                <slot></slot>
+              </gwds-grid>
             </div>
-            <gwds-grid perRow={this.perRow()}>
-              <slot></slot>
-            </gwds-grid>
           </div>
         </section>
       </Host>
